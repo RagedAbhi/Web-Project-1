90bee4004203fa9f8995900b2fb04f4c76323472
RagedAbhi <abhilash.khadanga@gmail.com>
2022-11-03 19:37:59 +0530

    second commit

---
+++ Web Development/JavaScript/Js 5.js
@@ -0,0 +1,16 @@
+console.log("This is javascript");
+
+let name1 = "Abhi";
+let name2 = "Abhi1";
+let name3 = "Abhi2";
+
+function greet(name){
+    console.log(name + " is the best.");
+}
+
+greet(name1)
+greet(name2)
+greet(name3)
+// console.log(name1 + " is the best.")
+// console.log(name2 + " is the best.")
+// console.log(name3 + " is the best.")
