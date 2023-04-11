fe5428a0ac8e2673500aff25fa6ae9651f8430b9
RagedAbhi <abhilash.khadanga@gmail.com>
2023-04-12 02:30:35 +0530

    Sociopedia almost done

--- Web dev practice projects/Sociopedia/server/controllers/users.js
+++ Web dev practice projects/Sociopedia/server/controllers/users.js
@@ -42,7 +42,7 @@ export const addRemoveFriend = async (req, res) => {
             friend.friends = friend.friends.filter((id) => id !== id)
         } else {
             user.friends.push(friendId)
-            friend.friens.push(id)
+            friend.friends.push(id)
         }
         await user.save()
         await friend.save()
